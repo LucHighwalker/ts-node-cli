59880b4b27fb32b99cbd90c7c8ce18c22e753bf1
LucHighwalker <luc.highwalker@gmail.com>
2019-02-20 21:46:15 -0800

    + bug fixes. + prints version with --version or -v.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 A simple command line tool to clone my [ts-node-starter](https://github.com/LucHighwalker/ts-node-starter).
+
+# Installation 
+
+```
+npm install -g ts-node-cli
+```
+
+# Usage
+
+```
+tsns [optional name]
+
+cd [given name or ts-node-starter]
+
+touch .env
+
+npm install
+
+tsc --watch
+
+nodemon (or whatever you use)
+```
+
+# Example .env file
+
+```
+HOST_URL='theurlyourserverison.com'
+PORT=4200
+
+MONGO_URL='mongoLocationURL'
+JWT_SECRET='averysecretsecret'
+
+MAILGUN_API_KEY='yourmailgunapikey'
+EMAIL_DOMAIN='mailgun.email.domain'
+```
--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@ const clone = require('git-clone');
 const sys = require('child_process');
 const exec = sys.exec;
 
+const package = JSON.parse(JSON.stringify(require('./package.json')));
+
 const [, , ...args] = process.argv;
 
 async function sh(cmd) {
@@ -22,11 +24,24 @@ async function main() {
     const name = args[0] ? args[0] : 'ts-node-startertest';
     const destination = `${process.cwd()}/${name}`;
 
-    clone('https://github.com/LucHighwalker/ts-node-starter', destination);
-    const _ = await sh(`cd ${destination} && git remote remove origin`);
-    console.log(
-      `Cloned starter pack to: ${destination}\n\nNext steps:\n  $ cd ${name}\n  $ npm install\n  $ tsc --watch`
-    );
+    if (name === '-v' || name === '--version') {
+      console.log(`ts-node-cli version: ${package.version}`);
+    } else {
+      clone(
+        'https://github.com/LucHighwalker/ts-node-starter',
+        destination,
+        async () => {
+          const resp = await sh(
+            `cd ${destination} && git remote remove origin`
+          );
+          if (resp) {
+            console.log(
+              `Cloned starter pack to: ${destination}\n\nNext steps:\n  $ cd ${name}\n  $ npm install\n  $ tsc --watch`
+            );
+          }
+        }
+      );
+    }
   } catch (error) {
     console.error(error);
   }
